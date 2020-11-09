b647b9995f9c5822c664671e4396789e77f31e1f
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2020-11-09 16:28:28 -0500

    ss

--- controllers/dbConecction.js
+++ controllers/dbConecction.js
@@ -13,4 +13,8 @@ const connection = mysql.createConnection({
   database : process.env.DATABASE
 });
 console.log(connection);
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 module.exports = connection;
